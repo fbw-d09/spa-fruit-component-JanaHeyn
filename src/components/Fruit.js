1c95a9ad64526c4ab5aadad7e1949cc24679206b
JanaHeyn <jana.heyn@web.de>
2023-06-22 23:05:51 +0200

    Solve Tasks

--- src/components/Fruit.js
+++ src/components/Fruit.js
@@ -1,20 +1,43 @@
-import React, {useState} from 'react'
+import React, {useState} from 'react';
+import './Fruit.scss';
 
-function Fruit() {
-   ////////Add you Code here 
-      
-    
+const colors = ["red", "blue", "green", "yellow", "purple"];
 
+function Fruit({name, onDelete}) {
+    const [ textColor, setTextColor ] =useState('');
+
+    const handleChangeColor = () => {
+        const newColor = colors[Math.floor(Math.random() *colors.length)];
+        setTextColor(newColor);
+        // console.log(newColor);
+    }
 
     return (
-        <div data-testid="fruit">
-            {/* add your code here */}
+        <div data-testid='fruit' className='Fruit'>
 
+            <p 
+                className='Fruit__text'
+                style={{color: textColor}}
+            >   {name}
+            </p>
 
+            <div className='buttonWrapper'>
+                <button 
+                    className='buttonWrapper__deleteBtn'
+                    onClick={onDelete}
+                >   
+                    delete
+                    <span>X</span>
+                </button>
 
-
-
-            
+                <button 
+                    className='buttonWrapper__changeColorBtn'
+                    onClick={handleChangeColor}
+                >
+                    change color 
+                    <span></span>
+                </button>    
+            </div>
         </div>
     )
 }
